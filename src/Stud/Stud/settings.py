787ff6e0b5465346b0dec96e3bc42fb9e7bb924f
nguyenduyan28 <nguyenduyan28@gmail.com>
2024-06-14 13:14:33 +0700

    init

--- src/Stud/Stud/settings.py
+++ src/Stud/Stud/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'room.apps.RoomConfig', # add this to start using app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
