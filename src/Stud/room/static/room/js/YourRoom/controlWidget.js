40af3ec052a73d7bdb17a7146b77acf64f61b3f5
Đỗ Lê Khoa <dolekhoa1407@gmail.com>
2024-06-25 21:22:48 +0700

    Update Your Room page: add clock widget(pomodoro and tracking section)

--- src/Stud/room/static/room/js/YourRoom/controlWidget.js
+++ src/Stud/room/static/room/js/YourRoom/controlWidget.js
@@ -1,4 +1,14 @@
-//Setup widget
+// HEADER SECTION
+//Fullscreen button
+document.querySelector('.zoomOut').addEventListener('click', function() {
+  if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen()
+  } else {
+      document.exitFullscreen();
+  }
+});
+
+// SETUP SECTION
 function setupWidget(openButton, widget, closeButton) {
   openButton.addEventListener('click', function() {
       if (widget.style.display === 'block') {
@@ -22,7 +32,29 @@ const openThemeButton = document.querySelector('.theme');
 const closeThemeWidget = document.querySelector('.closeThemeWidget');
 const themeWidget = document.querySelector('.themeWidget');
 
-//Interact theme selector
+// Music widget
+const openMusicButton = document.querySelector('.music');
+const musicWidget = document.querySelector('.musicContainer');
+const closeMusicWidget = document.querySelector('.closeMusicWidget');
+
+// Calendar
+const openCalendarButton = document.querySelector('.calendar');
+const calendarWidget = document.querySelector('.calendarContainer');
+const closeCalendarWidget = document.querySelector('.closeCalendarWidget');
+
+// Clock Widget
+const openClockButton = document.querySelector('.clock');
+const clockWidget = document.querySelector('.clockWidget');
+const closeClockWidget = document.querySelector('.closeClockWidget');
+
+// Setup widget
+setupWidget(openThemeButton, themeWidget, closeThemeWidget);
+setupWidget(openMusicButton, musicWidget, closeMusicWidget);
+setupWidget(openCalendarButton, calendarWidget, closeCalendarWidget);
+setupWidget(openClockButton, clockWidget, closeClockWidget);
+
+// INTERACT SECTION
+// Theme widget
 const imageBox =document.getElementsByClassName('imageBox');
 document.querySelectorAll('.imageBox').forEach(function(img) {
     img.addEventListener('click', function() {
@@ -32,30 +64,7 @@ document.querySelectorAll('.imageBox').forEach(function(img) {
     });
 });
 
-// Music widget
-const openMusicButton = document.querySelector('.music');
-const musicWidget = document.querySelector('.musicContainer');
-const closeMusicWidget = document.querySelector('.closeMusicWidget');
-
-//Calendar
-const openCalendarButton = document.querySelector('.calendar');
-const calendarWidget = document.querySelector('.calendarContainer');
-const closeCalendarWidget = document.querySelector('.closeCalendarWidget');
-
-// Setup widget
-setupWidget(openThemeButton, themeWidget, closeThemeWidget);
-setupWidget(openMusicButton, musicWidget, closeMusicWidget);
-setupWidget(openCalendarButton, calendarWidget, closeCalendarWidget);
-
-//Fullscreen button
-document.querySelector('.zoomOut').addEventListener('click', function() {
-    if (!document.fullscreenElement) {
-        document.documentElement.requestFullscreen()
-    } else {
-        document.exitFullscreen();
-    }
-});
-//Clock section
+// Calendar widget
 const timeElement = document.querySelector(".time");
 const dateElement = document.querySelector(".date");
 
@@ -95,8 +104,107 @@ setInterval(() => {
   dateElement.textContent = formatDate(now);
 }, 200);
 
-//Drag and Drop
+// Clock Widget
+const minutesInput = document.getElementById('minutes');
+const secondsInput = document.getElementById('seconds');
+const startBtn = document.querySelector('.startButton');
+const resetBtn = document.querySelector('.resetButton');
+const timerDisplay = document.querySelector('.pomodoro');
 
+const toggleSwitch = document.getElementById('toggle-switch');
+const trackingDisplay = document.getElementById('tracking');
+
+let intervalId;
+let trackingIntervalId;
+let isPomodoroRunning = false;
+let isTrackingRunning = false;
+
+let totalSeconds = 0;
+let trackingSeconds = 0;
+// Pomodoro
+function formatTimePomodoro(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+    const formattedSeconds = remainingSeconds.toString().padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+function playAudio() {
+  const audioElement = new Audio("/static/room/css/images/YourRoom/music/doneSound.mp3");
+  audioElement.play();
+}
+function startPomodoro() {
+    const minutes = parseInt(minutesInput.value);
+    const seconds = parseInt(secondsInput.value);
+
+    totalSeconds = minutes * 60 + seconds;
+    timerDisplay.textContent = formatTimePomodoro(totalSeconds);
+
+    intervalId = setInterval(() => {
+        totalSeconds--;
+
+        timerDisplay.textContent = formatTimePomodoro(totalSeconds);
+
+        // check timeout
+        if (totalSeconds === 0) {
+            clearInterval(intervalId);
+            isPomodoroRunning = false;
+
+            if (totalSeconds === 0) {
+                clearInterval(intervalId);
+                isPomodoroRunning = false;
+                playAudio();
+            }
+            minutesInput.value = 25;
+            secondsInput.value = 0;
+        }
+    }, 1000);
+
+    startBtn.disabled = true;
+    resetBtn.disabled = false;
+    isPomodoroRunning = true;
+}
+
+function resetPomodoro() {
+    clearInterval(intervalId);
+    totalSeconds = 0;
+
+    timerDisplay.textContent = formatTimePomodoro(totalSeconds);
+
+    startBtn.disabled = false;
+    resetBtn.disabled = true;
+    isPomodoroRunning = false;
+}
+// Tracking
+function startTracking() {
+    trackingIntervalId = setInterval(() => {
+        trackingSeconds++;
+        trackingDisplay.textContent = formatTimePomodoro(trackingSeconds);
+    }, 1000);
+
+    isTrackingRunning = true;
+}
+function stopTracking() {
+    clearInterval(trackingIntervalId);
+    isTrackingRunning = false;
+}
+toggleSwitch.addEventListener('change', () => {
+    if (toggleSwitch.checked) {
+        startTracking();
+    } else {
+        stopTracking();
+    }
+});
+
+startBtn.addEventListener('click', () => {
+  console.log('Start button clicked');
+  startPomodoro();
+});
+resetBtn.addEventListener('click', resetPomodoro);
+
+//Drag and drop
 function makeDraggable(draggableElement) {
   let isDragging = false;
   let initialX, initialY;
@@ -139,6 +247,8 @@ function makeDraggable(draggableElement) {
     isDragging = false;
   });
 }
+// Activate drag and drop to widget
 makeDraggable(themeWidget);
 makeDraggable(musicWidget);
 makeDraggable(calendarWidget);
+makeDraggable(clockWidget);
