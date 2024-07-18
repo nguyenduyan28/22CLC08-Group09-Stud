facbf91272c7d5c400465b9fa4096f18ca334dec
nguyenduyan28 <nguyenduyan28@gmail.com>
2024-07-18 14:20:37 +0700

    update config

--- src/Stud/Stud/settings.py
+++ src/Stud/Stud/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, "sroom/staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "room/staticfiles")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "account/static"),
